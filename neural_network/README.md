feb40fff4c0a3cd703114ea602f9008429d4f9f0
Cameron Fabbri <cameronfabbri@gmail.com>
2020-12-07 20:21:05 -0600

    adding pokemon data info

--- neural_network/README.md
+++
@@ -1,8 +0,0 @@
-# Neural Network
-The goal of the project is to classify digits from the MNIST dataset using a fully connected neural network
-written in Tensorflow
-
-## Project Goals
-- Understand numpy arrays and Tensorflow tensors
-
-
---
+++ pokemon/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://veekun.com/static/pokedex/downloads/generation-1.tar.gz
+wget https://veekun.com/static/pokedex/downloads/generation-2.tar.gz
+wget https://veekun.com/static/pokedex/downloads/generation-3.tar.gz
+wget https://veekun.com/static/pokedex/downloads/generation-4.tar.gz
+wget https://veekun.com/static/pokedex/downloads/generation-5.tar.gz
+
+mkdir pokemon/data
+mv gen*.tar.gz pokemon/data/
