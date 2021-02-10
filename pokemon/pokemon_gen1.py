fd7e70c59a8a0c0efb62c7022ddd786ccb12f6fa
Brian Heneghan <@BriHen>
2021-02-09 20:53:25 -0800

    backup pokedex file, changed gen1.py to use number as the index

--- pokemon/pokemon_gen1.py
+++ pokemon/pokemon_gen1.py
@@ -6,9 +6,17 @@ print('hello world p')
 
 
 
-data='Pokemon.csv'
+data='pokedex.csv'
 pokedex=pd.read_csv(data,index_col=0)
+#pokedex.rename(columns={"#":"Number"},inplace=True) # First time to change column name of "#"
+#pokedex.rename(str.lower, axis='columns', inplace=True)
 
-rand=random.randrange(0,len(pokedex))
+
+pokedex.to_csv('pokedex.csv', index=True)
+# rand=random.randrange(0,len(pokedex))     #Random on all values
+rand=random.randrange(0,151)                #151 pokemon in gen1
 print(rand)
 print(pokedex.iloc[rand])
+print('Last - 151')
+print(pokedex.loc[6])
+print(pokedex.head(20))
