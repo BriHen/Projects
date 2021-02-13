6c570fca6ce579f57288b99e1667c8b7c1413b77
Brian Heneghan <73624921+BriHen@users.noreply.github.com>
2021-02-13 14:02:37 -0800

    Update pokemon_gen1.py
    Added sprite file location, list of sprites, and list of sprite file paths

--- pokemon/pokemon_gen1.py
+++ pokemon/pokemon_gen1.py
@@ -1,13 +1,27 @@
 import pandas as pd
 import numpy as np
 import random
-from pokedex_gen import Gen1Pokedex
+import glob2
+import os
 
+# import functions
+from pokedex_generator import Gen1Pokedex
 
+#sprites is the location of sprite images for Gen1 pokemon
+sprites= "C:\\Users\\brima\\Documents\\CodeProjectData\\PokemonSprites\\pokemon\\main-sprites\\red-blue"
+
+#create pokedex for gen1 pokemon
 gen1=Gen1Pokedex()
 
-      
-      
-      
-      
-      
+#create list of sprite file names and sprite file paths
+spritename=[] # makes empty array for sprite file names
+spritepath=[]   #makes empty array for sprite file paths
+for file in os.listdir(sprites):
+    if file.endswith(".png"):
+        spritename.append(file)
+        spritepath.append(os.path.join(sprites,file))
+
+
+
+
+
