320e14b43316e839dab2dab374c07944a1f77245
Brian Heneghan <73624921+BriHen@users.noreply.github.com>
2021-02-13 14:00:50 -0800

    Update download.sh
    Change to add filepath on computer

--- pokemon/download.sh
+++ pokemon/download.sh
@@ -6,5 +6,5 @@ wget https://veekun.com/static/pokedex/downloads/generation-3.tar.gz;
 wget https://veekun.com/static/pokedex/downloads/generation-4.tar.gz;
 wget https://veekun.com/static/pokedex/downloads/generation-5.tar.gz;
 mkdir sprites;
-mv gen*.tar.gz Projects;
+mv gen*.tar.gz 'C:\\Users\\Brima\\Documents\\CodeProjectData\\PokemonSprites'
 
