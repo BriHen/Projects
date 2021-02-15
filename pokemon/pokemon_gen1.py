b1cfab1e3b72971be0d7f90820e90f850cdc222f
Brian Heneghan <73624921+BriHen@users.noreply.github.com>
2021-02-14 22:31:17 -0800

    Update pokemon_gen1.py
    Added array that is a concatenate of r g and b color channels

--- pokemon/pokemon_gen1.py
+++ pokemon/pokemon_gen1.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import glob2
 import os
+import cv2
 from sklearn import svm
 import imageio
 from PIL import Image
@@ -26,7 +27,7 @@ for file in os.listdir(sprites):
         spritepath.append(os.path.join(sprites,file))
 
 ## create a pd database with number and file location to sort
-file_db=pd.DataFrame({"Number":spritenumber,"Path":spritepath}) # create dataset
+file_db=pd.DataFrame({"Number":spritenumber,"Path":spritepath,"ImageArray":None}) # create dataset
 file_db=file_db.astype({'Number':'int32'})  #column number was imported as an object, changes to int 32
 file_db=file_db.sort_values(by='Number')    #sort by number
 
@@ -37,12 +38,35 @@ file_db=file_db.sort_values(by='Number')    #sort by number
 
 
 ## First find max size sprite, and save that as a variable to reshape all images
-maxpixel=0
-for file in spritepath:
-    im = imageio.imread(file)
-    if im.shape[1] > maxpixel:
-        maxpixel=im.shape[0]
-    print(im)
+pixel=0         #create variable pixel to be used for resizeing
+for file in spritepath:     
+    im = cv2.imread(file)   # uses c2v to read image file
+    if im.shape[1] > pixel: # All sprites are square, take one of the dimensions to save the largest value
+        pixel=im.shape[0]   # Save the largest value to pixel
+    #print(im)              # Tester
+
+
+## From Pixel size found, need to go in and resize each image
+for file in file_db['Path']:
+    print(file)
+    im = cv2.imread(file)
+    im = cv2.resize(im,(pixel,pixel),interpolation=cv2.INTER_LINEAR)
+    
+## using cv2, convert each image into the 3 channels, r, g, b
+rgb_temp=[]
+for file in file_db['Path']:
+    r, g, b = cv2.split(cv2.imread(file))   # seperates the three color channels, rgb 
+    b = np.asarray(b).reshape(-1)   # takes the array for the color and turns into an array
+    g = np.asarray(g).reshape(-1)
+    r = np.asarray(r).reshape(-1)
+    rgb_array=np.concatenate((r,g,b))   # combines arrays together, making one large array with three colors
+    
+
+cv2.imshow('image',im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#resized = cv2.resize(imgage1, (im,im), interpolation=cv2.INTER_LINEAR)
     
 
     
